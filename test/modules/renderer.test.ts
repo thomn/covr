2aa92eaa690a791d0f9cb5c4f2a46c72456f1145
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-09-29 12:15:54 +0200

    test(renderer): proper test

--- test/modules/renderer.test.ts
+++ test/modules/renderer.test.ts
@@ -4,21 +4,27 @@ import {renderer} from '../../src/modules';
 
 describe('covr', () => {
     describe('renderer', () => {
-        const file = join(process.cwd(), '/src/resources/badges/flat.svg');
+        const file = join(process.cwd(), '/test/fixtures/vector.svg');
 
-        it('should calculate total coverage', (done) => {
+        it('should render badge', (done) => {
             const {render} = renderer(file);
-
             const badge = render({
-                color: 'red',
-                key: 'foo',
-                value: 'bar',
-                leftWidth: '10',
-                rightWidth: '20',
-                padding: 5,
+                x: 2,
+                color_one: 'red',
+                color_two: 'blue',
+                color_three: 'green',
+                width: 10,
+                height: 10,
             });
 
-            assert(badge);
+            assert(badge === '<?xml version="1.0"?>\n' +
+                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">\n' +
+                '    <g style="fill-opacity:0.7; stroke:black; stroke-width:0.1cm;">\n' +
+                '        <circle cx="6" cy="2cm" r="100" style="fill:red;" transform="translate(0,50)"/>\n' +
+                '        <circle cx="6" cy="2cm" r="100" style="fill:blue;" transform="translate(70,150)"/>\n' +
+                '        <circle cx="6" cy="2cm" r="100" style="fill:green;" transform="translate(-70,150)"/>\n' +
+                '    </g>\n' +
+                '</svg>\n');
             done();
         });
     });
