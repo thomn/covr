a542ee8f7051a063d1c3caac2eba26be6bdd5460
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-09-29 12:14:01 +0200

    feat(middleware): cache header

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,7 @@
+export {default as cache} from './cache';
 export {default as container} from './container';
 export {default as context} from './context';
-export {default as sinkhole} from './sinkhole';
 export {default as database} from './database';
+export {default as sinkhole} from './sinkhole';
 export {default as logger} from './logger';
 export {default as debug} from './debug';
