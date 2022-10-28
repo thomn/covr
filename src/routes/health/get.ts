d7fa3e0e96709787e0e996a87cc949d94e926ea2
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-10-28 12:38:46 +0200

    feat(health): add memory consumption

--- src/routes/health/get.ts
+++ src/routes/health/get.ts
@@ -6,7 +6,17 @@ import {database} from '#/modules';
  * Date: 28.10.22
  * Time: 12:06
  */
-export default route(async () => ({
-    status: 'up',
-    database: database().status(),
-}));
+export default route(async () => {
+    const memory = {};
+    const used = process.memoryUsage();
+    for (const key in used) {
+        memory[key] = `${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`;
+    }
+
+    return ({
+        status: 'up',
+        database: database().status(),
+        uptime: process.uptime(),
+        memory,
+    });
+});
