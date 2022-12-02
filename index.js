d3000967f8d24b15db87fead798ecd72d9708807
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-12-02 13:41:20 +0100

    fix(index): terminate accordingly

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ require('./dist/apps/covr/index')
     .default()
     .catch()
 ;
+
+for (const signal of ['SIGINT', 'SIGTERM', 'SIGQUIT']) {
+    process.on(signal, () => {
+        process.exit(0);
+    });
+}
